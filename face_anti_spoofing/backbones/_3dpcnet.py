e6d4c02f848ced0f7dc28850653ea8767641cce7
Bernardo Biesseck <biesseck@gmail.com>
2023-08-29 20:40:28 -0300

    face_anti_spoofing/backbones/_3dpcnet.py: implement decoder using transposed covolutions (torch.nn.ConvTranspose2d)

--- face_anti_spoofing/backbones/_3dpcnet.py
+++ face_anti_spoofing/backbones/_3dpcnet.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn as nn
 
@@ -28,12 +29,31 @@ class _3DPCNet(nn.Module):
             from .iresnet2060 import iresnet2060
             self.encoder = iresnet2060(False, **kwargs)
 
-        self.decoder = self.get_decoder(self.face_embedd_size, self.num_output_points, self.num_axis)
+        # self.decoder = self.get_decoder_mlp(self.face_embedd_size, self.num_output_points, self.num_axis)
+        self.decoder = self.get_decoder_ConvTranspose2d(input_shape=(1, self.face_embedd_size), output_shape=(self.num_axis, self.num_output_points))
         self.classifier = self.get_classifier(self.num_output_points, self.num_axis, num_classes=2)
         self._initialize_weights()
 
 
-    def get_decoder(self, input_size=256, num_points=2500, num_axis=3):
+    def get_decoder_ConvTranspose2d(self, input_shape=(1, 256), output_shape=(3, 2500)):
+        layers = []
+
+        # layer 1
+        k_size = (output_shape[1]-input_shape[1]+1, 1)
+        layers.append(nn.ConvTranspose2d(in_channels=1, out_channels=129, kernel_size=k_size, stride=1))
+        # layers.append(nn.BatchNorm2d(129, eps=1e-05))
+        layers.append(nn.ReLU(True))
+
+        # layer 2
+        o_channels = output_shape[0]
+        layers.append(nn.ConvTranspose2d(in_channels=129, out_channels=o_channels, kernel_size=(1, 1), stride=1))
+        # layers.append(nn.BatchNorm2d(output_shape[0], eps=1e-05))
+        layers.append(nn.Tanh())
+
+        return nn.Sequential(*layers)
+
+
+    def get_decoder_mlp(self, input_size=256, num_points=2500, num_axis=3):
         layers = []
 
         # layer 1
@@ -68,20 +88,27 @@ class _3DPCNet(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
+            elif isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
 
     def forward(self, x):
         def _regress_pointcloud(x):
-            x = self.encoder(x)
-            x = self.decoder(x)
-            x = x.reshape(x.size(0), self.num_output_points, self.num_axis)
-            # print('_regress_pointcloud - x.size():', x.size())
+            x = self.encoder(x)              # x.shape = (batch, 256)
+            x = x.unsqueeze(1)               # x.shape = (batch, 1, 256)
+            x = x.unsqueeze(3)               # x.shape = (batch, 1, 256, 1)
+
+            x = self.decoder(x)              # x.shape = (batch, 3, 2500, 1)
+            x = x.squeeze(3)                 # x.shape = (batch, 3, 2500)
+            x = torch.permute(x, (0, 2, 1))  # x.shape = (batch, 2500, 3)
+
             return x
 
         def _get_logits(x):
             x = x.reshape(x.size(0), self.num_output_points*self.num_axis)
             logits = self.classifier(x)
-            # print('_get_logits - logits.size():', logits.size())
             return logits
 
         def _forward(x):
